50fc39ad72f7ac904f7510d263ae120fa90dc968
Pavel Petrov <doctorrosen2016@gmail.com>
2021-07-11 20:51:04 +0600

    create a Wialon.js

---
+++ src/modules/core/calculation/objects/glonass-systems/Wialon.js
@@ -0,0 +1,15 @@
+import Monitoring from "./Monitoring";
+
+export class Wialon extends Monitoring {
+    constructor() {
+        super();
+        this.name = 'Виалон';
+        this.addHandlers()
+    }
+
+    async logIn() {
+        
+    }
+
+    addHandlers()
+}
